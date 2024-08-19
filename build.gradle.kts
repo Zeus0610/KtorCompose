fe047ad278d1f3bf16f2c4c28e22dcfe8dab9983
Zeus Orea <zsrm1094@gmail.com>
2024-08-19 15:38:47 -0600

    reset agp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
     id("org.jetbrains.compose") version "1.6.10" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.0.0" apply false
-    id("com.android.application") version "8.5.1" apply false
-    id("com.android.library") version "8.5.1" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 }
